7182f826c7fcd251552a56f2a141b1530add7580
HARITS <harits.muhammad.only@gmail.com>
2022-09-08 09:08:32 +0700

    initial commit

---
+++ data type.py
@@ -0,0 +1,35 @@
+"""
+This program created to learn data type in python
+"""
+
+# Python use dynamic data types that are dynamic in nature and don't require initialization at the time of declaration
+# Some programming language use static data typed that require explicit definition of a data type
+# when they create a piece of data (e.g. variable, parameter, return value)
+# Example:
+# In Java
+# int mynumber = 4 //integer data type
+# string myname = 'Dudung' //string data type
+# float phi = 3.14 //float data type
+# In Python
+mydata = 4 #integer data type
+print(type(mydata))
+mydata = 'Dudung' #become string data type
+print(type(mydata))
+mydata = 3.14 #become float data type
+print(type(mydata))
+
+# Now we are focus on data type in python
+# Data types in python can be defined into 2 group based on application
+# Primitive data types and Non-Primitive data types
+
+# Primitive data type
+# Primitive data types are a set of basic data types from which all other data types are constructed.
+# It's used almost in every programming languages.
+# include:
+# interger, An integer data type represents some range of mathematical integers.
+# float, A floating-point number represents a single precision rational number that may have a fractional part.
+# boolean,  boolean is typically a logical type that can have either the value "true" or the value "false".
+# double, A floating-point number represents a double precision rational number that may have a fractional part.
+
+# Non Primitive data type
+#
