26591211182d2d7611169e188e012975ece55222
HARITS <harits.muhammad.only@gmail.com>
2022-09-10 07:27:35 +0700

    add contains

--- data type.py
+++ data type.py
@@ -1,22 +1,26 @@
 """
 This program created to learn data type in python
 """
+#Data Type
 
 # Python use dynamic data types that are dynamic in nature and don't require initialization at the time of declaration
 # Some programming language use static data typed that require explicit definition of a data type
 # when they create a piece of data (e.g. variable, parameter, return value)
 # Example:
+
 # In Java
-# int mynumber = 4 //integer data type
-# string myname = 'Dudung' //string data type
-# float phi = 3.14 //float data type
+# int mynumber = 4 //integer data type, mynumber variable is only accept integer data
+# string myname = 'Dudung' //string data type, myname variable is only accept string data
+# float phi = 3.14 //float data type, phy variable is only accept float data
+
 # In Python
-mydata = 4 #integer data type
+mydata = 4 #variable become integer data type
 print(type(mydata))
-mydata = 'Dudung' #become string data type
+mydata = 'Dudung' #variable become string data type
 print(type(mydata))
-mydata = 3.14 #become float data type
+mydata = 3.14 #variable become float data type
 print(type(mydata))
+
 
 # Now we are focus on data type in python
 # Data types in python can be defined into 2 group based on application
@@ -30,6 +34,15 @@ print(type(mydata))
 # float, A floating-point number represents a single precision rational number that may have a fractional part.
 # boolean,  boolean is typically a logical type that can have either the value "true" or the value "false".
 # double, A floating-point number represents a double precision rational number that may have a fractional part.
+# char, It is a data type that holds one character (letter, number, etc.).
+# But in python if you assign one character it would be considered as string data type
 
 # Non Primitive data type
-#
+# non-primitive data is not a basic data types that only store values,
+# but its a collection of values with one or more basic data types.
+# include:
+# interger, An integer data type represents some range of mathematical integers.
+# float, A floating-point number represents a single precision rational number that may have a fractional part.
+# boolean,  boolean is typically a logical type that can have either the value "true" or the value "false".
+# double, A floating-point number represents a double precision rational number that may have a fractional part.
+# char, It is a data type that holds one character (letter, number, etc.).
